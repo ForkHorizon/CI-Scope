f3a34c7b61bcce9e0ef40ed523c03e185f63819d
Daliys <daliys133@gmail.com>
2026-05-27 13:31:18 +0200

    Initial Commit

---
+++ CI Scope/CI_ScopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CI_ScopeApp.swift
+//  CI Scope
+//
+//  Created by Kiryl Shcherba on 27/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct CI_ScopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
